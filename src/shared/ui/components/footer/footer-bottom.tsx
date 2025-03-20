c77dc95f71c4a716fc376c64248678cbe463f767
daniil-b2be <dan.developer@born2be.media>
2025-03-20 15:46:31 +0200

    content

--- src/shared/ui/components/footer/footer-bottom.tsx
+++ src/shared/ui/components/footer/footer-bottom.tsx
@@ -19,8 +19,8 @@ export function FooterBottom() {
           <Link href="tel:+00 000 000 00">
             <Text color="black60">+00 000 000 00</Text>
           </Link>
-          <Link href="mailto:example@gmail.com">
-            <Text color="black60">example@gmail.com</Text>
+          <Link href="mailto:info@marketifire.com">
+            <Text color="black60">info@marketifire.com</Text>
           </Link>
         </div>
         <SocialNetworks />
