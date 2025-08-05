c56a707bf94d3ffdd4c8e4e4baf2c4ea0e1f81d4
daniil-b2be <dan.developer@born2be.media>
2025-08-05 15:43:39 +0300

    inst x

--- src/shared/ui/components/social-networks/social-networks.tsx
+++ src/shared/ui/components/social-networks/social-networks.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 
-import { FacebookIcon } from '@/shared/ui/icons/fill';
+import { FacebookIcon, InstagramIcon, XIcon } from '@/shared/ui/icons/fill';
 
 import st from './social-networks.module.scss';
 
@@ -10,16 +10,19 @@ export function SocialNetworks({ iconsSize = 18 }: { iconsSize?: number }) {
   return (
     <nav aria-label="Social media links">
       <ul className={st.socialNetworks}>
-        {/* <li>
-          <Link href="https://x.com/" aria-label="x">
+        <li>
+          <Link href="https://x.com/marketifirecom" aria-label="x">
             <XIcon width={iconsSize} height={iconsSize} />
           </Link>
         </li>
         <li>
-          <Link href="https://www.instagram.com/" aria-label="instagram">
+          <Link
+            href="https://www.instagram.com/marketifire/"
+            aria-label="instagram"
+          >
             <InstagramIcon width={iconsSize} height={iconsSize} />
           </Link>
-        </li> */}
+        </li>
         <li>
           <Link
             href="https://www.facebook.com/marketifirecom/"
