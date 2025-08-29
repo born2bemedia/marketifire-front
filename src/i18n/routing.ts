5905f17cc0143ccb063841f09e2baf525b69097f
Daniil <dan.developer@born2be.media>
2025-08-29 18:09:36 +0300

    [feature/primary-lang]: Primary Lang (#71)

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en', 'de', 'it', 'sk'],
-  defaultLocale: 'en',
+  locales: ['sk', 'en', 'de', 'it'],
+  defaultLocale: 'sk',
 });
